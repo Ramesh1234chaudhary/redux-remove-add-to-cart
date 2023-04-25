96708adacb0dc7568aa6226f4d38ac8197f87932
ramesh <romanchaudhary01994@gmail.com>
2023-04-26 04:36:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+  import  "./App.css";
+   import Nav from './Component/Nav';
+  import Home from './Component/Home';
+  import {Routes,Route} from 'react-router-dom'
+  import Product from './Component/Product';
+  import Cart from './Component/Cart';
+  
+const App = () => {
+  return (
+    <div  className='App'>
+        <Nav/>
+
+    <Routes>
+               <Route path="/" element={<Home/>}/>
+
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path="/Product" element={<Product/>}/>
+</Routes>
+        
+
+
+    
+       
+
+
+      </div>
+
+  )
+}
+
+export default App
+
